1093d3ee71b81517050d6e39f2da8f123a8246e4
Duc Hien Nguyen <nguyenduch@mediamarktsaturn.com>
2020-07-05 02:56:39 +0200

    problem 1

--- Project_2/problem_1.py
+++ Project_2/problem_1.py
@@ -31,7 +31,7 @@ class LRU_Cache(object):
 
         
 class Test(unittest.TestCase):
-    def test_given_queue_6_5_2_1_4__321_remove_key_3(self):
+    def test_given_queue_6_5_2_1_4__321_then_remove_key_3(self):
         print('test_given_queue_6_5_2_1_4_remove_key_3')
         our_cache = LRU_Cache(5)
         our_cache.set(1, 1)
@@ -48,7 +48,7 @@ class Test(unittest.TestCase):
 
         self.assertEqual(-1, our_cache.get(3))     # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
 
-    def test_given_queue_2_6_5_3_4__432_1_remove_key_1(self):
+    def test_given_queue_2_6_5_3_4__432_1_then_remove_key_1(self):
         print('test_given_queue_2_6_5_3_4remove_key_1')
         our_cache = LRU_Cache(5)
         our_cache.set(1, 1)
@@ -66,7 +66,7 @@ class Test(unittest.TestCase):
         self.assertEqual(2, our_cache.get(2))    
         self.assertEqual(-1, our_cache.get(1))   # returns -1 because the cache reached it's capacity and 1 was the least recently used entry
 
-    def test_given_queue_65_3443_1221_remove_key_2(self): 
+    def test_given_queue_65_3443_1221_then_remove_key_2(self): 
         print('test_given_queue_2653_4_43_21_21_remove_key_2')
         our_cache = LRU_Cache(5)
         our_cache.set(1, 1)
